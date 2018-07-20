57b98aecd9c24d5216d7f80476ad9196d8c2a530
ivoferro <ivoferro1995@gmail.com>
2018-07-20 12:04:11 +0100

    Implements section 4

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {title: "Javascript", pages: 100},
+        {title: "Elixir", pages: 350},
+        {title: "Java", pages: 150},
+        {title: "Python", pages: 200}
+    ]
+}
